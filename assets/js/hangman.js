13fc5702a84ffce169f1cdf8dc23187efefe9759
janebmckenna <>
2023-10-03 06:49:01 +0000

    style: update styling on images

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -3,12 +3,12 @@ let incorrectGuess = 0;
 let maxGuesses = 8;
 let correctLetters = [];
 
-function newGame(){
+function newGame() {
     newWord();
     resetGame();
 };
 
-function resetGame(){
+function resetGame() {
     incorrectGuess = 0;
     correctLetters = [];
     document.getElementById('hangman-image').innerHTML = `<img src="assets/images/hangmanImg0.png" alt="Hangman Image">`;
@@ -17,9 +17,13 @@ function resetGame(){
     document.getElementById('loss').style.display = ('none');
     $('.hint').hide();
 };
-
-function correctLetter(letter){
-    for (let i =0; i<= currentWord.length; i++){
+/**
+ * 
+ * @param {string} letter letter chosen by the user
+ * @returns victory messge if the letter completes the hangman game
+ */
+function correctLetter(letter) {
+    for (let i = 0; i <= currentWord.length; i++) {
         if (letter === currentWord.at(i)) {
             document.getElementsByTagName('li')[i].innerHTML = currentWord.at(i);
             let add = correctLetters.push(letter);
@@ -29,45 +33,45 @@ function correctLetter(letter){
     if (correctLetters.length === currentWord.length) return displayVictory();
 };
 
-function wrongLetter(){
+function wrongLetter() {
     incorrectGuess++;
-    document.getElementById('hangman-image').innerHTML = `<img src="assets/images/hangmanImg` + incorrectGuess +`.png" alt="Hangman Image">`;
+    document.getElementById('hangman-image').innerHTML = `<img src="assets/images/hangmanImg` + incorrectGuess + `.png" alt="Hangman Image">`;
     if (incorrectGuess === maxGuesses) return displayLoss();
 };
 
-function displayVictory(){
+function displayVictory() {
     document.getElementById('win').style.display = ('flex');
     document.getElementById('win-text').innerHTML = `That right, the word was: ${currentWord}`;
 };
 
-function displayLoss(){
+function displayLoss() {
     document.getElementById('loss').style.display = ('flex');
     document.getElementById('loss-text').innerHTML = `The word was: ${currentWord}`;
 };
 
-function newWord(){
-    let {word, hint} = wordList[Math.floor(Math.random() * wordList.length)];
+function newWord() {
+    let { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
     currentWord = word;
     $('.hint').html(hint);
     $('.letters').html(word.split('').map(() => `<li class="letter"></li>`).join(''));
 };
-newWord();
 
-$('.keyboard > button').click(function(e){
+$('.keyboard > button').click(function (e) {
     $(this).attr('disabled', true);
     checkLetter(this.innerHTML);
-})
+});
 
-function checkLetter(clickedLetter){
-   if(currentWord.includes(clickedLetter)){
-    correctLetter(clickedLetter);
-   } else {
-    wrongLetter(clickedLetter);
-   }
+function checkLetter(clickedLetter) {
+    if (currentWord.includes(clickedLetter)) {
+        correctLetter(clickedLetter);
+    } else {
+        wrongLetter(clickedLetter);
+    }
 };
 
-$('.hint-title').click(function(){
+$('.hint-title').click(function () {
     $('.hint').toggle('fast');
-})
+});
 
-console.log(wordList.length)
+newWord();
+console.log(wordList.length);
